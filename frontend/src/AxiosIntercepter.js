5c2b30ce2e6c24f81df9655e85d89a2d5565bc27
Prashant Kachhore <prashant.kachhore@bytesfarms.com>
2025-04-08 15:30:06 +0530

    Initial commit

---
+++ frontend/src/AxiosIntercepter.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:3500',
+})
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            console.log('call the refresh token api here')
+            // Handle 401 error, e.g., redirect to login or refresh token
+        }
+        return Promise.reject(error)
+    },
+)
+
+export default axiosInstance
